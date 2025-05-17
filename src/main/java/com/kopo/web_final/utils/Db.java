2afb100d45989615bf128da5f58b0e15f3e7a931
심규원 <abs3011@konkuk.ac.kr>
2025-05-17 12:57:00 +0900

    fix: home

--- src/main/java/com/kopo/web_final/utils/Db.java
+++ src/main/java/com/kopo/web_final/utils/Db.java
@@ -4,22 +4,23 @@ import javax.naming.Context;
 import javax.naming.InitialContext;
 import javax.sql.DataSource;
 import java.sql.Connection;
+import java.sql.DriverManager;
 
 public class Db {
-    private static DataSource ds;
+
+    private static final String URL = "jdbc:oracle:thin:@dinkdb_medium?TNS_ADMIN=C:/dev/web_final/Wallet_DinkDB";
+    private static final String USERNAME = "DA2507";
+    private static final String PASSWORD = "Data2507";
 
     static {
         try {
-            Context initContext = new InitialContext();
-            Context envContext = (Context) initContext.lookup("java:/comp/env");
-            ds = (DataSource) envContext.lookup("jdbc/OracleDB");
-        } catch (Exception e) {
+            Class.forName("oracle.jdbc.OracleDriver"); // 드라이버 로드
+        } catch (ClassNotFoundException e) {
             e.printStackTrace();
         }
     }
 
     public static Connection getConnection() throws Exception {
-        return ds.getConnection();
+        return DriverManager.getConnection(URL, USERNAME, PASSWORD);
     }
-
 }
